a7e910a7f8e50d0b00b1b581aec887426e3fe25f
xXCassX <94416086+xXCassX@users.noreply.github.com>
2022-11-20 19:39:54 -0600

    Revert "io"
    This reverts commit 6937b56b141828c58289939a54ab49fc0de3c137.

---
+++ MyRestaurant/Entidades/Platillos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRestaurant.Entidades
+{
+    class Platillos : Restaurantes
+    {
+        public string descripcionP { get; set; }
+        public string precioP { get; set; }
+        public string iconoP { get; set; }
+    }
+}
---
+++ MyRestaurant/Modelos/PlatillosModel.cs
@@ -0,0 +1,39 @@
+﻿using MyRestaurant.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace MyRestaurant.Modelos
+{
+    public class PlatillosModel
+    {
+        
+        ObservableCollection<Platillos> LPlatillos {get; set;}
+
+        public PlatillosModel()
+        {
+            LPlatillos = new ObservableCollection<Platillos>
+            {
+                new Platillos
+                {
+                    descripcionP = "plato 1",
+                    precioP = "$14.45",
+                    iconoP = "https://i.ibb.co/0cpFpv4/IMG01.png"
+                },
+                new Platillos
+                {
+                    descripcionP = "plato 2",
+                    precioP = "$64.59",
+                    iconoP = "https://i.ibb.co/bBS7Lh2/IMG03.png"
+                },
+                new Platillos
+                {
+                    descripcionP = "plato 3",
+                    precioP = "$23.45",
+                    iconoP = "https://i.ibb.co/H4J330b/IMG04.png"
+                }
+            };        
+        } 
+    }
+}
